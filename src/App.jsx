13752dc7a34a2a096e3880070bf84ad32b1a5c1a
Haku Clement <haku.clement@gmail.com>
2025-08-29 10:37:41 +0100

    Minor Edits

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,8 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import AboutShartinga from './pages/AboutShartinga'
-import SizeSelect from './components/SizeSelect'
-import ClothingPage from './components/ClothingPage'
-import HoodiePhoto from './assets/pictures/guy_in_darkgrey_hoodie.jpg'
-import CartIcon from './components/CartIcon'
-import LuxClothingPage from './components/FashionItemPage'
 import Homepage from './pages/Homepage'
-import FashionCard from './components/FashionCard'
-import SnookerModel from './assets/pictures/model_at_snooker2.jpg'
-import FrameCard from './components/FrameCard'
 import ExplorePage from './pages/ExplorePage'
-import ScrollToTop from './components/ScrollToTop'
 import FashionItemPage from './components/FashionItemPage'
 
  
@@ -21,12 +12,8 @@ const router = createBrowserRouter([
   {path: '/', element: <Homepage />}, 
   {path: '/explore', element: <ExplorePage />}, 
   {path: '/product/:itemId', element: <FashionItemPage 
-    src={HoodiePhoto} name="Hoodie De Shartinga" />},
-  {path: '/about', element: <AboutShartinga />},
-   
-
-  {path: '/test3', element: <CartIcon />},    
-
+    name="Hoodie De Shartinga" />},
+  {path: '/about', element: <AboutShartinga />}   
 ])
 
 
