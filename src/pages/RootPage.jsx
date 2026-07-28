bbdda3236f83e7ffd314af5a99e7fd07c28536d8
Haku <ultimatejustin20@gmail.com>
2026-07-28 10:36:53 +0100

    [frontend] Shartinga Clock Logo made into a loading icon when routes change

--- src/pages/RootPage.jsx
+++ src/pages/RootPage.jsx
@@ -1,11 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import ScrollToTop from "../components/ScrollToTop";
 import Footer from "../components/Footer"
 import MainHeader from "../components/NewHeader";
 
 function RootPage() {
+    const navigation = useNavigation();
+    const isLoading = navigation.state === "loading";
+
     return (
         <>
+            {isLoading && (
+                <div className="routeLoadingOverlay" aria-live="polite" aria-busy="true">
+                    <img
+                        src="/images/SM_LOGO_WHITEONBLACK.PNG"
+                        alt="Loading"
+                        className="routeLoadingImage"
+                    />
+                </div>
+            )}
             <ScrollToTop />
             <MainHeader />
             <Outlet />
